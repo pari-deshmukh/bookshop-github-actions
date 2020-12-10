c3858b7fdbca6e0cbf451e439a106b51afc0574d
Pranali Deshmukh <deshmuk3@coventry.ac.uk>
2020-12-10 15:11:47 +0000

    test(books): adds unit tests for add, get and getAll methods
    adds unit test cases for the add, get and getAll methods of the books module
    
    ref #20

--- unitTests/test-books.js
+++ unitTests/test-books.js
@@ -1,6 +1,33 @@
 import test from 'ava'
 import { Books } from '../modules/books.js'
 
+const merchantOfVenice = {
+	name: 'The Merchant of Venice',
+	author: 'William Shakespeare',
+	description: 'An early work of Shakespeare.',
+	price_mu: 860,
+	ean: '978-9380816296',
+	type: 'paperback',
+	condition: 'new',
+	qty: 25,
+	weight_gm: 154,
+	image_thumbnail_name: 'merchant_of_venice_thumbnail',
+	image_fullsize_name: 'merchant_of_venice_full'
+}
+const juliusCaesar = {
+	name: 'Julius Caesar',
+	author: 'William Shakespeare',
+	description: 'Another early work of Shakespeare.',
+	price_mu: 132,
+	ean: '978-8129101914',
+	type: 'paperback',
+	condition: 'new',
+	qty: 12,
+	weight_gm: 75,
+	image_thumbnail_name: 'julius_caesar_thumbnail',
+	image_fullsize_name: 'julius_caesar_full'
+}
+
 test('DB : connect to database and check tables', async test => {
 	test.plan(1)
 	try{
@@ -13,15 +40,89 @@ test('DB : connect to database and check tables', async test => {
 	}
 })
 
-test('GET : get all books', async test => {
+test('ADD : add a new book', async test => {
 	test.plan(1)
 	const books = await new Books()
-	const data = await books.getAll()
-	test.truthy(data)
+	const addBook = await books.add(merchantOfVenice)
+	test.is(typeof addBook, 'object', 'unable to add book')
 	books.close()
 })
 
-test('GET : error if incorrect book id', async test => {
+test('ADD : error if blank book', async test => {
+	test.plan(1)
+	const books = await new Books()
+	try {
+		await books.add({})
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'book object is empty', 'incorrect error message')
+	} finally {
+		books.close()
+	}
+})
+
+test('ADD : error if blank book name', async test => {
+	test.plan(1)
+	const books = await new Books()
+	try {
+		await books.add({name: '', author: 'John Doe'})
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing book name', 'incorrect error message')
+	} finally {
+		books.close()
+	}
+})
+
+test('ADD : error if undefined book name', async test => {
+	test.plan(1)
+	const books = await new Books()
+	try {
+		await books.add({author: 'John Doe'})
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing book name', 'incorrect error message')
+	} finally {
+		books.close()
+	}
+})
+
+test('ADD : error if duplicate book', async test => {
+	test.plan(1)
+	const books = await new Books()
+	try {
+		await books.add(merchantOfVenice)
+		await books.add(merchantOfVenice)
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message,
+			'book "The Merchant of Venice" by author "William Shakespeare" is already added', 'incorrect error message')
+	} finally {
+		books.close()
+	}
+})
+
+test('GET : get all books', async test => {
+	test.plan(1)
+	const books = await new Books()
+	await books.add(merchantOfVenice)
+	await books.add(juliusCaesar)
+	const data = await books.getAll()
+	test.is(data.length, 2, 'incorrect response length')
+	books.close()
+})
+
+test('GET : add book and retrieve by id', async test => {
+	test.plan(1)
+	const books = await new Books()
+	const bookRecord = await books.add(merchantOfVenice)
+	const record = await books.get(bookRecord.lastID)
+	delete record.id
+	test.deepEqual(record, merchantOfVenice, 'book not found by id')
+	books.close()
+})
+
+test('GET : invalid book id', async test => {
 	test.plan(1)
 	const books = await new Books()
 	try{
@@ -34,8 +135,5 @@ test('GET : error if incorrect book id', async test => {
 	}
 })
 
-test.todo('GET: get all books')
-test.todo('GET: get book by id')
-test.todo('ADD: add book')
 test.todo('UPDATE: update book by id')
 test.todo('REMOVE: delete book by id')

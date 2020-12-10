bc187004d43c56a3b6e562e37771fe37966e2a46
Pranali Deshmukh <deshmuk3@coventry.ac.uk>
2020-12-10 11:28:52 +0000

    feat(books): adds getAll and get methods to books module
    adds the unit tests and get methods for all books and book by id
    
    ref #20

--- modules/books.js
+++ modules/books.js
@@ -5,22 +5,50 @@ class Books {
 		return (async() => {
 			this.db = await sqlite.open(dbName)
 			// we need this table to store the user accounts
-			const bookSql = 'CREATE TABLE IF NOT EXISTS books\
+			let sql = 'CREATE TABLE IF NOT EXISTS books\
         (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,\
         author TEXT, description TEXT);'
-			await this.db.run(bookSql)
-			const bookTypeSql = 'CREATE TABLE IF NOT EXISTS bookType\
+			await this.db.run(sql)
+			sql = 'CREATE TABLE IF NOT EXISTS bookType\
         (id INTEGER PRIMARY KEY AUTOINCREMENT, book_id INTEGER NOT NULL,\
         price_mu INTEGER, ean TEXT, type TEXT, condition TEXT,\
         qty INTEGER, weight_gm INTEGER,\
         FOREIGN KEY (book_id) REFERENCES books (id));'
-			await this.db.run(bookTypeSql)
-			const bookImagesSql = 'CREATE TABLE IF NOT EXISTS bookImages\
+			await this.db.run(sql)
+			sql = 'CREATE TABLE IF NOT EXISTS bookImages\
         (id INTEGER PRIMARY KEY AUTOINCREMENT, book_id INTEGER NOT NULL,\
         image_thumbnail_name TEXT, image_fullsize_name TEXT);'
-			await this.db.run(bookImagesSql)
+			await this.db.run(sql)
 			return this
 		})()
+	}
+
+	/**
+	 * gets all books
+	 * @returns [Object] returns an array of book objects if the books have been added
+	 */
+	async getAll() {
+		const sql = 'SELECT * FROM books;'
+		const data = await this.db.run(sql)
+		await this.db.run(sql)
+		return data
+	}
+
+	/**
+	 * gets a book by it's id
+	 * @param {String} id the id to check
+	 * @returns Object returns object with the book details if the book exists
+	 */
+	async get(id) {
+		let sql = `SELECT count(id) AS count FROM books WHERE id="${id}";`
+		const records = await this.db.get(sql)
+		if(!records.count) throw new Error(`book id "${id}" not found`)
+		sql = `SELECT * FROM books WHERE id="${id}"\
+    INNER JOIN bookType ON bookType.bookid = books.id\
+    INNER JOIN bookImages ON bookImages.bookid = books.bookid;`
+		const data = await this.db.run(sql)
+		await this.db.run(sql)
+		return data
 	}
 
 	async close() {
--- unitTests/test-books.js
+++ unitTests/test-books.js
@@ -4,12 +4,33 @@ import { Books } from '../modules/books.js'
 test('DB : connect to database and check tables', async test => {
 	test.plan(1)
 	try{
-		const book = await new Books() // no database specified so runs in-memory
+		const books = await new Books()
 		test.pass('Database connection successful')
-		book.close()
+		books.close()
 	}	catch(err) {
 		console.log(err)
 		test.fail('Database connection failed')
+	}
+})
+
+test('GET : get all books', async test => {
+	test.plan(1)
+	const books = await new Books()
+	const data = await books.getAll()
+	test.truthy(data)
+	books.close()
+})
+
+test('GET : error if incorrect book id', async test => {
+	test.plan(1)
+	const books = await new Books()
+	try{
+		await books.get(12345678)
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'book id "12345678" not found', 'incorrect error message')
+	} finally {
+		books.close()
 	}
 })
 
